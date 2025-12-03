2bd8fb3d4c694ceee4d3f3ea7d3066cc1a090514
Dim.Roumpekas <jimroumpi@gmail.com>
2025-12-03 03:08:55 +0200

    feat: add subapp

---
+++ src/gr/aueb/cf/testgit/SubApp.java
@@ -0,0 +1,12 @@
+package gr.aueb.cf.testgit;
+
+public class SubApp {
+   public static void main(String[] args) {
+       System.out.println(subApp(10,5));
+
+    }
+
+    public static int subApp(int a, int b){
+       return a - b;
+    }
+}
